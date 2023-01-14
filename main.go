2d9cb2efff7c549705f058a67053955154bba955
Apiwat Supaksirakol <api.supak@gmail.com>
2023-01-14 22:02:21 +0700

    hard mode interface

--- main.go
+++ main.go
@@ -2,36 +2,18 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"io"
+	"os"
 )
 
-type shape interface {
-	area() float64
-}
-type square struct {
-	length float64
-}
-
-type triangle struct {
-	base   float64
-	height float64
-}
-
 func main() {
-	sq := square{5.25}
-	tri := triangle{base: 1.00, height: 2.00}
-
-	printArea(sq)
-	printArea(tri)
-}
-
-func (tr triangle) area() float64 {
-	return 0.5 * tr.base * tr.height
-}
-func (sq square) area() float64 {
-	return sq.length * sq.length
-}
-
-func printArea(sh shape) {
-	fmt.Println(reflect.TypeOf(sh), sh.area())
+	readArg := os.Args[1]
+	fmt.Println("Open File:", readArg)
+	contentWrites, err := os.Open(readArg)
+	if err != nil {
+		fmt.Printf("Error opening file: %v", err)
+		os.Exit(1)
+	}
+	io.Copy(os.Stdout, contentWrites)
+	fmt.Println()
 }
